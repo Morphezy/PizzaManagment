923564858c945db1778bef71f3757ff4a991a693
Morphezy <nicebrohehe@gmail.com>
2025-07-25 20:04:13 +0300

    Came back to work

---
+++ Infrastructure/Migrations/20250719190235_pendingOrders.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class pendingOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Diameter",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "Egg",
+                table: "Pizzas");
+
+            migrationBuilder.CreateTable(
+                name: "PendingOrders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PendingOrders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PendingOrders_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PendingOrders_OrderId",
+                table: "PendingOrders",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PendingOrders");
+
+            migrationBuilder.AddColumn<float>(
+                name: "Diameter",
+                table: "Pizzas",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Egg",
+                table: "Pizzas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
